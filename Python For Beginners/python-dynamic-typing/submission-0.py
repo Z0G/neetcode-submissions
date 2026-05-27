1acd2c7bd8f7b64817a8611c1fc7a1ec8f7987c7
Mohammad Shoumik Raihan <37881170+Z0G@users.noreply.github.com>
2026-05-26 21:59:42 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 10
+print(type(variable))
+variable = 10.2
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Not very good at coding"
+print(type(variable))
+variable = [5,6,7]
+print(type(variable))
